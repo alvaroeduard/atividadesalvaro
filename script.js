a7739c58a33471546008f340c7aa081be8234f12
alvaro <catzs.2022112linf0030@aluno.ifpi.edu.br>
2024-09-01 22:09:27 -0300

    01/09/2024 22:09

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ document.getElementById("formLogin").addEventListener("submit", function(event) 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    
     fetch('credenciais.json')
         .then(response => response.json())
         .then(data => {
             const user = data.users.find(user => user.username === username && user.password === password);
             
             if (user) {
-                
+                // Criar um token e armazená-lo
+                const token = 'seu_token_unico_aqui';
+                localStorage.setItem('authToken', token);
                 window.location.href = "repositorio.html";
             } else {
-                
                 document.getElementById("loginMessage").textContent = "Usuário ou senha inválidos!";
             }
         })
