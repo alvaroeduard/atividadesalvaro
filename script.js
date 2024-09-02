8bb023bd22ae646c8397358eac76169c00a52f7a
alvaro <catzs.2022112linf0030@aluno.ifpi.edu.br>
2024-09-01 21:11:52 -0300

    01/09/2024 21:11

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.getElementById("formLogin").addEventListener("submit", function(event) {
-    event.preventDefault(); // Previne o envio do formulário
+    event.preventDefault();
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Solicita as credenciais do arquivo JSON
+    
     fetch('credenciais.json')
         .then(response => response.json())
         .then(data => {
             const user = data.users.find(user => user.username === username && user.password === password);
             
             if (user) {
-                // Credenciais corretas, redireciona para a página index.html
+                
                 window.location.href = "index.html";
             } else {
-                // Credenciais incorretas, exibe mensagem de erro
+                
                 document.getElementById("loginMessage").textContent = "Usuário ou senha inválidos!";
             }
         })
